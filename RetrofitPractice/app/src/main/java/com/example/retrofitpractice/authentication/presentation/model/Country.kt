839912e38dfcc30c8fc900997c84c8f845e19a0f
SIDRNH <dshanmukha10@gmail.com>
2025-04-02 17:05:19 +0530

    Added Different Countries Phone Settings

--- RetrofitPractice/app/src/main/java/com/example/retrofitpractice/authentication/presentation/model/Country.kt
+++ RetrofitPractice/app/src/main/java/com/example/retrofitpractice/authentication/presentation/model/Country.kt
@@ -1,5 +1,17 @@
 package com.example.retrofitpractice.authentication.presentation.model
 
-enum class Country(val countryIso: String, val countryName: String, val countryCode: String) {
-    INDIA("In", "India", "+91")
-}
+data class Country(
+    val name: String,
+    val emoji: String,
+    val isoCode: String,
+    val code: String,
+    val limit: Int
+)
+
+val countries: List<Country> = listOf(
+    Country("India", "\uD83C\uDDEE\uD83C\uDDF3", "IN", "+91", 10),
+    Country("United States","\uD83C\uDDFA\uD83C\uDDF8", "US", "+1", 10),
+    Country("United Kingdom","\uD83C\uDDEC\uD83C\uDDE7", "GB", "+44", 10),
+    Country("Germany","\uD83C\uDDE9\uD83C\uDDEA", "DE", "+4945", 13),
+    Country("Canada","\uD83C\uDDE8\uD83C\uDDE6", "CA", "+1", 10)
+)

809ce2d6448db36f8f41ff83af9cfda523d829e7
SIDRNH <sidrnh@gmail.com>
2025-04-25 17:45:38 +0530

    Improved UI a little bit

---
+++ Yes Chat/app/src/main/java/com/example/yeschat/home/ChannleItem.kt
@@ -0,0 +1,52 @@
+package com.example.yeschat.home
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun ChannelItem(channelName: String, onClick: () -> Unit) {
+    Row(
+        modifier = Modifier.fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 2.dp)
+            .clip(RoundedCornerShape(16.dp))
+            .background(Color.Gray)
+            .clickable{
+                onClick();
+            },
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Box(
+            modifier = Modifier.padding(8.dp)
+                .size(70.dp)
+                .clip(CircleShape)
+                .background(Color.Yellow.copy(alpha = 0.3f)),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = channelName[0].uppercase(),
+                fontSize = 35.sp,
+                color = Color.White
+            );
+        }
+        Text(
+            text = channelName,
+            modifier = Modifier.padding(8.dp),
+            color = Color.White
+        )
+    }
+}
--- Yes Chat/app/src/main/java/com/example/yeschat/home/HomeScreen.kt
+++ Yes Chat/app/src/main/java/com/example/yeschat/home/HomeScreen.kt
@@ -1,7 +1,6 @@
 package com.example.yeschat.home
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -9,9 +8,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
@@ -19,8 +22,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.yeschat.ChatScreen
@@ -39,7 +44,8 @@ fun HomeScreen(navController: NavController, state: HomeScreenState, onEvent: (H
                     )
                 }
             )
-        }
+        },
+        containerColor = Color.Black
     ) { innerPadding ->
         Box(
             modifier = Modifier
@@ -47,19 +53,28 @@ fun HomeScreen(navController: NavController, state: HomeScreenState, onEvent: (H
                 .padding(innerPadding)
         ) {
             LazyColumn {
-                items(state.channels) { channel ->
-                    Text(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(8.dp)
+                item {
+                    Text(text = "Messages", color = Color.Gray, fontSize = 20.sp, fontWeight = FontWeight.Black, modifier = Modifier.padding(16.dp));
+                }
+                item {
+                    OutlinedTextField(
+                        value = "",
+                        onValueChange = {},
+                        placeholder = { Text(text = "Search") },
+                        modifier = Modifier.fillMaxWidth()
+                            .padding(vertical = 8.dp, horizontal = 16.dp)
                             .clip(RoundedCornerShape(16.dp))
-                            .background(Color.Red.copy(alpha = 0.3f))
-                            .clickable {
-                                navController.navigate(ChatScreen(channelId = channel.id))
-                            }
-                            .padding(16.dp),
-                        text = channel.name
+                            .background(Color.Gray),
+                        trailingIcon = {
+                            Icon(imageVector = Icons.Default.Search, contentDescription = "Search")
+                        }
                     )
+                }
+                items(state.channels) { channel ->
+                    ChannelItem(
+                        channelName = channel.name,
+                        onClick = {navController.navigate(ChatScreen(channelId = channel.id))}
+                    );
                 }
             }
         }
